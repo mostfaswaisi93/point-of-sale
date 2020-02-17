f1057165f91a3b4f873827e4a66c09027ea43db6
mostfaswaisi93 <mostfaswaisi@gmail.com>
2020-02-17 13:26:33 +0200

    Add Admin Panel

---
+++ app/Http/Controllers/Admin/Client/OrderController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Client;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class OrderController extends Controller
+{
+    //
+}
